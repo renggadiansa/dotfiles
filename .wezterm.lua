d282b76899726abf8fa85a99ca0668a82542f9f0
renggadiansa <renggadiansa@gmail.com>
2024-11-30 11:28:01 +0700

    edited config .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,7 +19,7 @@ config.colors = {
 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
---config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = false })
 config.font_size = 12
 
 config.enable_tab_bar = false
